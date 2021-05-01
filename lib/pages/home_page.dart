cc215e3dd918aa23e31183719eef5d03a4af7a8b
AliGharian <ali.gharian98@gmail.com>
2021-05-01 22:50:19 +0430

    make the basic envirement

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,12 +2,51 @@ import 'package:flutter/material.dart';
 import 'package:tricky_spin/utils/constants.dart';
 
 class HomePage extends StatelessWidget {
+  final Color backgroundColor = CustomColors.blue_bg;
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
-      backgroundColor: CustomColors.yellow_bg,
-      body: Center(
-        child: Text('Tricky Spin'),
+      backgroundColor: backgroundColor,
+      body: Stack(
+        fit: StackFit.expand,
+        alignment: Alignment.center,
+        children: [
+          Positioned(
+            top: size.height * 0.15,
+            child: Text(
+              '10',
+              style: TextStyle(
+                fontSize: 64.0,
+                color: CustomColors.light_white,
+              ),
+            ),
+          ),
+          Positioned(
+            left: 0.0,
+            right: 0.0,
+            child: Container(
+              width: size.width * 0.3,
+              height: size.height * 0.3,
+              decoration: BoxDecoration(
+                color: Colors.grey.withOpacity(0.8),
+                shape: BoxShape.circle,
+              ),
+            ),
+          ),
+          Positioned(
+            left: 0.0,
+            right: 0.0,
+            child: Container(
+              width: size.width * 0.15,
+              height: size.height * 0.15,
+              decoration: BoxDecoration(
+                color: backgroundColor,
+                shape: BoxShape.circle,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
